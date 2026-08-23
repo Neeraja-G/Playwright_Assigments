9de5c01401a31c5bf05f96e88172d3aa8aba4e30
Gaddam Neeraja <neerajagaddam76@gmail.com>
2026-08-23 13:11:23 +0530

    updated PW CF as main

--- Web Automation Exercise/playwright.config.js
+++ Web Automation Exercise/playwright.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    video:'off'
+    screenshot:'off'
   },
 
   /* Configure projects for major browsers */
@@ -79,4 +79,3 @@ export default defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
